193352bbd3f1e25bf0ce25029a838ce11c55da6e
Ameer Arsala <ameer.arsala03@gmail.com>
2025-01-01 20:13:35 -0800

    fresh 6

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const docs = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(), // the page title
+    description: z.string(),
     pubDatetime: z.date(),
     modDatetime: z.date(),
     draft: z.boolean(),
